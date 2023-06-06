0cf0a5c9ad75e922695bf29db15e70f766f288ba
Bhakti Jadhav <81022744+BhaktiJ338@users.noreply.github.com>
2023-06-06 10:42:53 +0530

    Create D3_mergeIntervals.cpp

---
+++ D3_mergeIntervals.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
+{
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>>ans;
+    ans.push_back(intervals[0]);
+
+    for(int i=1; i<intervals.size(); i++){
+        if(intervals[i][0]<=ans.back()[1]){
+            ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+        }
+        else{
+            ans.push_back(intervals[i]);
+        }
+    }
+    return ans;
+}
