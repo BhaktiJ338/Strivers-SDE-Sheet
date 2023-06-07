0a5aa84698894900869968482cb1e7eb9f713b39
Bhakti Jadhav <81022744+BhaktiJ338@users.noreply.github.com>
2023-06-07 21:05:20 +0530

    Create D4_modularExponentiation.cpp

---
+++ D4_modularExponentiation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int modularExponentiation(int x, int n, int m) {
+	long ans=1;
+	long xx=x;
+	
+	while(n>0){
+		if(n%2!=0){
+			ans=((ans)%m*(xx)%m)%m;
+		}
+		xx=((xx)%m*(xx)%m)%m;
+		n /= 2;
+	}
+	
+	return (int)(ans % m);
+
+}
