441a66a094be41e813df12235f7747b1f39ae04b
Bhakti Jadhav <81022744+BhaktiJ338@users.noreply.github.com>
2023-07-08 18:24:49 +0530

    Create D27_nextGreaterElement.cpp

---
+++ D27_nextGreaterElement.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+
+vector<int> nextGreater(vector<int> &arr, int n) {
+    stack <int>st;
+    vector<int>nge(n, -1);
+
+    for(int i=n-1; i>=0; i--){
+        while(!st.empty() && st.top()<=arr[i]) st.pop();
+
+        if(!st.empty()) nge[i] = st.top();
+        
+        st.push(arr[i]);
+    }
+    
+    return nge;
+}
